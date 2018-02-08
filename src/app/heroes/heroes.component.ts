73517d784a4bd135e4e9723124033c9184187cd5
Dr. Lionel Saliou <lionel.saliou@gmail.com>
2018-02-08 18:05:11 +0000

    Implementing Angular routing
    In terms of approach, this is very similar to the way Django does
    it. Indeed, we define URLs and point to specific view(s) [in
    this case component templates].
    
    The styling is bit odd at the moment because of the use of links
    but it is likely to be solved shortly with better CSS.
    
    As expected, we finally split the master and detail view completely.
    Now, the master view does not know about this view at all. Now,
    it is the responsibily of the "routing" to figure out what to
    do when the user click on links.
    
       -- Lionel Saliou, Ph.D

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,18 +10,12 @@ import { Observable } from 'rxjs/Observable';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes: Hero[] ;
+
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
-  }
-
-  heroes: Hero[] ;
-
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero ;
   }
 
   getHeroes(): void {
